e1ec9e460120aa2150e7f553d1bd26de62be03fe
uladzimir <01myit@gmail.com>
2023-11-13 21:33:44 +0300

    Used methods: createStore(from Redux), getState and dispatch(both from store).

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import { StrictMode } from 'react' 
 import { createRoot } from 'react-dom/client' 
-
-const initialState = 0
+import { createStore } from 'redux'
+const initialState = 10
 const reducer = (state = 0, action) => {
   switch(action.type) {
     case 'INC': return state = state + 1 // need 'return'
     default: return state
   }
 }
-let state = reducer(undefined, { type: 'INC', })
-state = reducer(state, { type: 'INC', })
-state = reducer(state, { type: 'INC', })
-state = reducer(state, { type: 'INC', })
-state = reducer(state, { type: 'INC', })
-console.log(state)
+const store = createStore(reducer)
+console.log(store.getState())
+store.dispatch({ type: 'INC' })
+console.log(store.getState())
+store.dispatch({ type: 'INC' })
+console.log(store.getState())
+store.dispatch({ type: 'INC' })
+console.log(store.getState())
+// let state = reducer(undefined, { type: 'INC', })
+// state = reducer(state, { type: 'INC', })
+// state = reducer(state, { type: 'INC', })
+// state = reducer(state, { type: 'INC', })
+// state = reducer(state, { type: 'INC', })
+// console.log(state)
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
 
