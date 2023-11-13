5f2b0cca03c1ba61560c61d4b762c54681c45f6b
uladzimir <01myit@gmail.com>
2023-11-13 21:15:48 +0300

    An example with undefined.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client' 
 
 const initialState = 0
-const reducer = (state, action) => {
-  if (action.type === 'INC') {
-    return state = state + 1
+const reducer = (state = 0, action) => {
+  switch(action.type) {
+    case 'INC': return state = state + 1 // need 'return'
+    default: return state
   }
-  return 0
 }
-let state = reducer(initialState, { type: 'INC', })
+let state = reducer(undefined, { type: 'INC', })
 state = reducer(state, { type: 'INC', })
 state = reducer(state, { type: 'INC', })
 state = reducer(state, { type: 'INC', })
