3809798eac067434de0f7f46c1a546acc32f9f91
uladzimir <01myit@gmail.com>
2023-11-13 20:45:20 +0300

    MY COMMIT

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react' 
+import { createRoot } from 'react-dom/client' 
+console.log('Hello Redux!')
+createRoot(document.querySelector('#root')).render(
+  <StrictMode>
+
+  </StrictMode>
+) 
+
