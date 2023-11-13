ffa9535545a4e02c39fe052406b72d561b4d9582
uladzimir <01myit@gmail.com>
2023-11-13 21:09:14 +0300

    A Simple example of redux working.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import { StrictMode } from 'react' 
 import { createRoot } from 'react-dom/client' 
-console.log('Hello Redux!')
+
+const initialState = 0
+const reducer = (state, action) => {
+  if (action.type === 'INC') {
+    return state = state + 1
+  }
+  return 0
+}
+let state = reducer(initialState, { type: 'INC', })
+state = reducer(state, { type: 'INC', })
+state = reducer(state, { type: 'INC', })
+state = reducer(state, { type: 'INC', })
+state = reducer(state, { type: 'INC', })
+console.log(state)
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
 
