aa0d3b26c97425763d44393c1ad14dc573d23d6f
uladzimir <01myit@gmail.com>
2023-11-15 21:39:10 +0300

    Little code refactoring.

--- src/index.js
+++ src/index.js
@@ -9,15 +9,22 @@ const { dispatch, getState, subscribe } = store
 // function bindActionCreator(dispatch, actionCreator, ...args) { //analog of the function bindActionCreators from Redux
   // return dispatch(actionCreator(...args))
 // }
-const incDispatch = bindActionCreators(increment, dispatch)
-const decDispatch = bindActionCreators(decrement, dispatch)
-const rndDispatch = bindActionCreators(() => random((Math.random() * 100).toFixed()), dispatch )
+// const incDispatch = bindActionCreators(increment, dispatch)
+// const decDispatch = bindActionCreators(decrement, dispatch)
+// const rndDispatch = bindActionCreators(() => random((Math.random() * 100).toFixed()), dispatch)
+const actionsDispatch = bindActionCreators({
+  increment,
+  decrement,
+  random, 
+}, dispatch)
 subscribe(() => {
   document.querySelector('.counter').innerText = getState().counter
 })
-document.querySelector('#inc').addEventListener('click', incDispatch)
-document.querySelector('#dec').addEventListener('click', decDispatch )
-document.querySelector('#random').addEventListener('click', rndDispatch)
+document.querySelector('#inc').addEventListener('click', actionsDispatch.increment)
+document.querySelector('#dec').addEventListener('click', actionsDispatch.decrement )
+document.querySelector('#random').addEventListener('click', () => {
+  actionsDispatch.random((Math.random() * 100).toFixed())
+})
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
 
