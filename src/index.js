482f24c0b51617cc627e228f3acee7375d0aae59
uladzimir <01myit@gmail.com>
2023-11-16 20:50:28 +0300

    Code refactoring.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react' 
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-import Counter from './Counter'
-import { store } from './store'
+import App from './App'
+import { store } from './store/store'
 import './index.css'
 createRoot(document.querySelector('#root')).render(
   <StrictMode>
     <Provider store={store}>
-      <Counter />
+      <App />
     </Provider>
   </StrictMode>
 ) 
